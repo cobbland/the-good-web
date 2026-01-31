8c158f7ed4268e767f33b83c4f4cf5cbf0ab307c
Jacob Densford <hello@jacobdensford.com>
2026-01-31 17:45:23 -0500

    add updateHTML function and fix getFeeds

--- scripts/getFeeds.js
+++ scripts/getFeeds.js
@@ -2,14 +2,23 @@ const Parser = require('rss-parser');
 const parser = new Parser();
 const cheerio = require('cheerio');
 const fs = require('fs/promises');
-const page = 'weekly/index.html';
+const weekly = 'weekly/index.html';
+const daily = 'daily/index.html';
 const feeds = {
     blogs: [
+        'https://blog.saisarida.com/feed.xml',
+        'https://julia.densford.net/feed.xml',
+    ],
+    news: [
+        'https://feeds.npr.org/1001/rss.xml',
+        'https://www.aljazeera.com/xml/rss/all.xml',
+    ],
+    me: [
         'https://cobb.land/feed.xml',
-    ]
+    ],
 };
 
-async function getFeeds(feedsInput = feeds, age = 7, count = 6) {
+async function getFeeds(feedsInput, age, count) {
     let allFeeds = {};
     const toDate = new Date();
     toDate.setUTCDate(toDate.getUTCDate() - age);
@@ -30,43 +39,74 @@ async function getFeeds(feedsInput = feeds, age = 7, count = 6) {
                 .slice(0, count)
                 .map((post) => {
                     return {
-                        postTitle: post.title || post.link,
+                        postTitle: post.title || 'Untitled Post',
                         postLink: post.link
                     }
                 });
-            allFeeds[key].push({
+            const site = {
                 siteTitle: feed.title || feed.link,
                 siteLink: feed.link || feed.feedUrl,
                 posts: posts,
-            });
+            };
+            if (site.posts.length > 0) {
+                allFeeds[key].push(site);
+            }
+        }
+    }
+    for (const topic of Object.keys(allFeeds)) {
+        if (allFeeds[topic].length <= 0) {
+            delete allFeeds[topic];
         }
     }
     return allFeeds;
 }
 
-async function updateHTML(htmlPath = page) {
-    const posts = await getFeeds();
+async function updateHTML(htmlPath, feedsInput, age, count) {
+    const posts = await getFeeds(feedsInput, age, count);
     const html = await fs.readFile(htmlPath, 'utf-8');
     const $ = cheerio.load(html);
     const nav = $('#nav > ul');
     const content = $('#content');
     nav.empty();
     content.empty();
-    Object.keys(posts).forEach(key => {
+    Object.keys(posts).forEach((topic) => {
         nav.append(`
-            <li><a href="#${key}">${key}</a></li>
+            <li><a href="#${topic}">${topic}</a></li>
         `);
         content.append(`
-            <div id="${key}" class="topic">
-                <h2><a href="#the-good-web">${key}</a></h2>
+            <div id="${topic}" class="topic">
+                <h2><a href="#the-good-web">${topic}</a></h2>
                 <div class="feeds">
 
                 </div>
             </div>
-        `)
-        const feeds = $(`#${key} > .feeds`);
-        // iterate through posts an append to feeds above
-    })
+        `);
+        const feeds = $(`#${topic} > .feeds`);
+        posts[topic].forEach((feed) => {
+            let feedPosts = ``;
+            feed.posts.forEach((post) => {
+                feedPosts = feedPosts + `
+                    <li class="post">
+                        <a href="${post.postLink}">
+                            ${post.postTitle}
+                        </a>
+                    </li>
+                `
+            });
+            feeds.append(`
+                <div class="feed">
+                    <h3 class="feed-name">
+                        <a href="${feed.siteLink}">${feed.siteTitle}</a>
+                    </h3>
+                    <ul class="posts">
+                        ${feedPosts}
+                    </ul>
+                </div>
+            `);
+        });
+    });
+    await fs.writeFile(htmlPath, $.html(), 'utf-8');
 }
 
-updateHTML();
+updateHTML(weekly, feeds, 8, 5);
+updateHTML(daily, feeds, 2, 5);
